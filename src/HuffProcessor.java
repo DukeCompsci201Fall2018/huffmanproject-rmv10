0cb1276494279cce1cacb69ba4dbe18878c6c9e1
Radu Vasilescu <vasilescur@gmail.com>
2018-12-06 01:34:16 -0500

    Implemented HuffProcessor.decompress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -37,13 +37,16 @@ public class HuffProcessor {
 	 * @param out Buffered bit stream writing to the output file.
 	 */
 	public void compress(BitInputStream in, BitOutputStream out) {
-
 		while (true) {
 			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1)
+
+			if (val == -1) {
 				break;
+			}
+
 			out.writeBits(BITS_PER_WORD, val);
 		}
+
 		out.close();
 	}
 
@@ -55,13 +58,80 @@ public class HuffProcessor {
 	 * @param out Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out) {
+		// Check if the file starts with the Huffman Coding magic number
+		int firstBits = in.readBits(BITS_PER_INT);
+		if (firstBits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with " + firstBits);
+		}
+
+		// Read the tree header
+		HuffNode root = readTreeHeader(in);
+
+		// Process the bits and write them out
+		decompressBits(in, out, root);
+
+		out.close();
+	}
+
+	/**
+	 * Reads the Huffman Tree header of a compressed file.
+	 * 
+	 * @param in file stream
+	 * @return the root node of the tree
+	 */
+	private HuffNode readTreeHeader(BitInputStream in) {
+		// Read a single bit
+		int bit = in.readBits(1);
+
+		if (bit == -1) {
+			throw new HuffException("Bit is -1");
+		}
+
+		// If internal --> Recursively read left then right
+		if (bit == 0) {
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0, 0, left, right);
+		} else {	// Leaf --> Read value
+			// Read nine bits from input
+			int value = in.readBits(BITS_PER_WORD);
+			return new HuffNode(value, 0, null, null);
+		}
+	}
+
+	/**
+	 * Decompresses a Huffman-encoded bit stream and writes it out.
+	 * 
+	 * @param in compressed bit stream
+	 * @param out target for uncompressed data
+	 */
+	private void decompressBits(BitInputStream in, BitOutputStream out, HuffNode root) {
+		HuffNode current = root;
 
 		while (true) {
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1)
-				break;
-			out.writeBits(BITS_PER_WORD, val);
+			// Read next bit
+			int bits = in.readBits(1);
+
+			if (bits == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			} else {
+				if (bits == 0) {
+					current = current.myLeft;
+				} else {
+					current = current.myRight;
+				}
+
+				// If current is a leaf node...
+				if (current.myLeft == null && current.myRight == null) {
+					if (current.myValue == PSEUDO_EOF) {
+						break;	// Stop processing
+					} else {
+						// Write bits for current value
+						out.write(current.myValue);
+						current = root;
+					}
+				}
+			}
 		}
-		out.close();
 	}
 }
